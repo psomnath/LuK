e612c8c15d60f3d5968c0c97bb5e138ad8c80bb5
Debanjana Baksi <debaksi@microsoft.com>
2021-10-07 19:52:32 +0530

    mobile-app

---
+++ src/LukMobileApp/LukMobileApp/LuK/INotificationManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuK
+{
+    public interface INotificationManager
+    {
+        event EventHandler NotificationReceived;
+        void Initialize();
+        void SendNotification(string title, string message);
+        void ReceiveNotification(string title, string message);
+    }
+}
